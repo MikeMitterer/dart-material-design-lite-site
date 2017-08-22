7db40187cebcb7322ff56c14e08357b47adc7b7c
Mike Mitterer <office@mikemitterer.at>
2017-08-22 17:02:10 +0200

    doc: Sample for MaterialTranslate added

---
+++ samples/mdld_translate/lib/locale/messages.dart
@@ -0,0 +1,19 @@
+library l10n.locale;
+
+/**
+* DO NOT EDIT. This is code generated with:
+*     projectdir $ mkl10llocale .
+*/
+
+import 'package:l10n/l10n.dart';
+
+
+final L10NTranslate translate = new L10NTranslate.withTranslations( {
+   "en": {
+      "Translate me": "Translate me"
+   },
+   "de": {
+      "Translate me": "Übersetze mich"
+   }
+});
+
---
+++ samples/mdld_translate/web/main.dart
@@ -0,0 +1,81 @@
+import 'package:logging/logging.dart';
+import 'package:console_log_handler/console_log_handler.dart';
+import 'package:dice/dice.dart' as di;
+
+import 'package:intl/intl.dart';
+import 'package:intl/intl_browser.dart';
+import 'package:l10n/l10n.dart';
+
+import 'package:mdl/mdl.dart';
+import 'package:mdl/mdlobservable.dart';
+
+import 'package:mdl_translate_sample/locale/messages.dart';
+
+//final L10NTranslate translate = new L10NTranslate.withTranslations( {
+//    "de": {
+//        "Translate me": "Übersetze mich"
+//    },
+//
+//    "en": {
+//        "Translate me": ""
+//    }
+//});
+
+@Component
+class Application extends MaterialApplication {
+    //final Logger _logger = new Logger('main.Application');
+
+    final ObservableProperty<bool> checkAttribute = new ObservableProperty<bool>(false);
+
+    Application() {
+    }
+
+    @override
+    void run() {
+
+    }
+
+    //- private -----------------------------------------------------------------------------------
+
+}
+
+main() async {
+    final Logger _logger = new Logger('main.Application');
+
+    configLogging();
+
+    final String locale = await findSystemLocale();
+    translate.locale = Intl.shortLocale(locale);
+
+    registerMdl();
+
+    _logger.info("Locale: ${locale}");
+    
+    final MaterialApplication application = await componentFactory().
+        rootContext(Application)
+        .addModule(new SampleModule())
+        .run();
+
+    application.run();
+}
+
+
+/**
+ * Demo Module
+ */
+class SampleModule extends di.Module {
+    configure() {
+        bind(Translator).toInstance(translate);
+    }
+}
+
+void configLogging() {
+    hierarchicalLoggingEnabled = false; // set this to true - its part of Logging SDK
+
+    // now control the logging.
+    // Turn off all logging first
+    Logger.root.level = Level.INFO;
+    Logger.root.onRecord.listen(new LogConsoleHandler());
+}
+
+
